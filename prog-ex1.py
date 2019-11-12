cfe80283458a42dc40c5f1a454ff44d0ae665a4a
Anselm von Wangenheim <anselm.w@gmx.de>
2019-11-12 17:54:24 +0100

    implement assemble_stiffness_local

--- prog-ex1.py
+++ prog-ex1.py
@@ -16,13 +16,24 @@ def gen_mesh(n):
         else: # odd
             return [lower_left, lower_left + n+2, lower_left + n+1]
     elements = list(map(corners, range(0, 2*n*n)))
-    dirichletboundary = set(range(0, n+1)) | set(range(n+1, n*(n+1), n+1)) | set(range(2*n+1, (n+1)*(n+1), n+1)) | set(range(n*(n+1), (n+1)*(n+1), 1))
+    dirichletboundary = set(range(0, n+1)) | set(range(n+1, n*(n+1), n+1)) | \
+            set(range(2*n+1, (n+1)*(n+1), n+1)) | set(range(n*(n+1), (n+1)*(n+1), 1))
     return (nodes, coordinates, elements, dirichletboundary)
 
 #print(gen_mesh(3))
+nodes, coordinates, elements, dirichletboundary = gen_mesh(3)
 
-def assemble_stiffness_local(a0, a1, a2):
-    42
+def assemble_stiffness_local(triangle):
+    g1 = np.array([[1] + coordinates[c] for c in elements[triangle]])
+    g2 = np.array([[0, 0], [1, 0], [0, 1]])
+    g = np.linalg.inv(np.transpose(g1)).dot(g2)
+    x = g1[:,1]
+    y = g1[:,2]
+    a = 1/2 * abs((x[1]-x[0])*(y[2]-y[0]) - (y[1]-y[0])*(x[2]-x[0])) * \
+            g.dot(np.transpose(g))
+    return a
+
+print(assemble_stiffness_local(0))
 
 def assemble_mass_local():
     42
